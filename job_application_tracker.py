ac4fc490a813738e6f78232eb8d7a8fcca0ad9b1
chase0aks <123683526+chase0aks@users.noreply.github.com>
2023-09-21 15:31:47 -0500

    Update job_application_tracker.py

--- job_application_tracker.py
+++ job_application_tracker.py
@@ -1,88 +1,117 @@
 import json
-
-# Initialize a list to store job application data
-job_applications = []
-
-# Load data from the JSON file (if it exists)
-try:
-  with open('job_applications.json', 'r') as json_file:
-    job_applications = json.load(json_file)
-except FileNotFoundError:
-  # If the JSON file doesn't exist, start with an empty list
-  job_applications = []
+import os
 
 
-# Function to save job applications data to a JSON file
-def save_job_applications():
+# Function to load job applications from a JSON file
+def load_job_applications():
+  try:
+    with open('job_applications.json', 'r') as json_file:
+      job_applications = json.load(json_file)
+  except (json.JSONDecodeError, FileNotFoundError):
+    job_applications = []
+
+  if not job_applications:
+    print("Welcome to the Job Application Tracker!")
+    print("No job applications found. Let's add your first job application.")
+    add_initial_job(job_applications)
+    save_job_applications(job_applications)
+
+  return job_applications
+
+
+# Function to add an initial job application
+def add_initial_job(job_applications):
+  job_data = {
+      'Job Code': 'Job Code',
+      'Job Title': 'Job Title',
+      'Company': 'Company',
+      'Application Date': 'Application Date',
+  }
+  # Collect user input for each job data field
+  new_application = {
+      key: input(f"Enter {value}: ")
+      for key, value in job_data.items()
+  }
+  new_application['Response Status'] = ["Applications", "No Response"]
+  job_applications.append(new_application)
+
+
+# Function to save job applications to a JSON file
+def save_job_applications(job_applications):
   with open('job_applications.json', 'w') as file:
     json.dump(job_applications, file)
 
 
-# Function to add a new application
-def add_application(job_code, job_title, company, app_date):
-  new_application = {
-      'Job Code': job_code,
-      'Job Title': job_title,
-      'Company': company,
-      'Application Date': app_date,
-      'Response Status': ["Applications",
-                          "No Response"]  # Default response statuses
+# Function to remove a job application by Job Code
+def remove_application(job_code, job_applications):
+  job_applications = [
+      application for application in job_applications
+      if application['Job Code'] != job_code
+  ]
+  save_job_applications(job_applications)
+  if not job_applications:
+    print(f"No job applications found with Job Code {job_code}.")
+  else:
+    print(f"Application with Job Code {job_code} has been removed.")
+
+
+# Function to add a new job application
+def add_application(job_applications):
+  job_data = {
+      'Job Code': 'Job Code',
+      'Job Title': 'Job Title',
+      'Company': 'Company',
+      'Application Date': 'Application Date',
   }
-
+  # Collect user input for each job data field
+  new_application = {
+      key: input(f"Enter {value}: ")
+      for key, value in job_data.items()
+  }
+  new_application['Response Status'] = ["Applications", "No Response"]
   job_applications.append(new_application)
-
-  # Save the updated data to the JSON file
-  save_job_applications()
-
+  save_job_applications(job_applications)
   print("Application added successfully!")
 
 
-# Function to update an existing application
-def update_application(job_code, new_response_status):
+# Function to update the response status of a job application by Job Code
+def update_application(job_code, new_response_status, job_applications):
   for application in job_applications:
     if application['Job Code'] == job_code:
-      response_statuses = application.get('Response Status', [])
-
+      response_statuses = application.setdefault('Response Status', [])
       if response_statuses and response_statuses[-1] == 'No Response':
         response_statuses[-1] = new_response_status
       else:
         response_statuses.append(new_response_status)
-
-      # Save the updated data to the JSON file
-      save_job_applications()
-
+      save_job_applications(job_applications)
       print("Application updated successfully!")
       return
+  print(f"No job application found with Job Code {job_code}.")
 
-  print("Job not found!")
 
+# Function to print all job applications
+def print_all_applications(job_applications):
+  if not job_applications:
+    print("No job applications found.")
+    return
 
-# Function to print all applications with their last response status
-def print_all_applications():
-  for application in job_applications:
-    job_code = application.get('Job Code', '')
-    job_title = application.get('Job Title', '')
-    company = application.get('Company', '')
-    app_date = application.get('Application Date', '')
-    response_statuses = application.get('Response Status', [])
-
-    # Get the last response status (if any)
-    last_response = response_statuses[-1] if response_statuses else ''
-
-    print(f"Job Code: {job_code}")
-    print(f"Job Title: {job_title}")
-    print(f"Company: {company}")
-    print(f"Application Date: {app_date}")
-    print(f"Last Response Status: {last_response}")
+  print("\nAll Job Applications:")
+  for i, application in enumerate(job_applications, 1):
+    print(f"\nApplication {i}:")
+    for key, value in application.items():
+      print(f"{key}: {value}")
     print()
 
 
-# Function to print response pairs
-def print_response_pairs():
-  print("\n")
-  response_counts = {}  # Dictionary to store response status counts
+# Function to print response status pairs
+def print_response_pairs(job_applications):
+  if not job_applications:
+    print("No job applications found.")
+    return
 
-  # Iterate through applications and their response statuses
+  print("\nResponse Status Pairs:")
+  response_counts = {}
+
   for application in job_applications:
     response_statuses = application.get('Response Status', [])
     if response_statuses:
@@ -90,38 +119,84 @@ def print_response_pairs():
         pair = f"{response_statuses[i]} [{response_statuses[i + 1]}]"
         response_counts[pair] = response_counts.get(pair, 0) + 1
 
-  # Display the response pairs and their counts in the middle
   for pair, count in response_counts.items():
     response1, response2 = pair.split('[')
     response2 = response2.rstrip(']')
     print(f"{response1} [{count}] {response2}")
 
 
-# Main menu loop
+# Function to search for job applications based on a category and value
+def search_application(category, value, job_applications):
+  found_applications = []
+  if category == 1:  # Search by Job Code
+    key = 'Job Code'
+  elif category == 2:  # Search by Job Title
+    key = 'Job Title'
+  elif category == 3:  # Search by Company
+    key = 'Company'
+  elif category == 4:  # Search by Application Date
+    key = 'Application Date'
+  else:
+    return found_applications  # Invalid category
+
+  found_applications = [
+      app for app in job_applications if key in app and app[key] == value
+  ]
+  return found_applications
+
+
+# Function to print search results
+def print_search_results(results):
+  if not results:
+    print("No applications found matching the search criteria.")
+    return
+
+  print("\nSearch Results:")
+  for i, application in enumerate(results, 1):
+    print(f"\nResult {i}:")
+    for key, value in application.items():
+      print(f"{key}: {value}")
+    print()
+
+
+# Load job applications from file
+job_applications = load_job_applications()
+
+# Menu loop for user interaction
 while True:
   print("\nMenu:")
   print("1. Add an application")
   print("2. Update an application")
   print("3. See all applications")
   print("4. Print response pairs")
-  print("5. Exit")
+  print("5. Remove an application")
+  print("6. Search for an application")
+  print("7. Exit")
   choice = input("Enter your choice: ")
 
   if choice == '1':
-    job_code = input("Enter Job Code: ")
-    job_title = input("Enter Job Title: ")
-    company = input("Enter Company: ")
-    app_date = input("Enter Application Date: ")
-    add_application(job_code, job_title, company, app_date)
+    add_application(job_applications)
   elif choice == '2':
     job_code = input("Enter Job Code to update: ")
     new_response = input("Enter new response: ")
-    update_application(job_code, new_response)
+    update_application(job_code, new_response, job_applications)
   elif choice == '3':
-    print_all_applications()
+    print_all_applications(job_applications)
   elif choice == '4':
-    print_response_pairs()
+    print_response_pairs(job_applications)
   elif choice == '5':
+    job_code = input("Enter Job Code to remove: ")
+    remove_application(job_code, job_applications)
+  elif choice == '6':
+    category = int(
+        input(
+            "Search by category:\n1. Job Code\n2. Job Title\n3. Company\n4. Application Date\nEnter your choice: "
+        ))
+    search_value = input("Enter the value to search for: ")
+    found_applications = search_application(category, search_value,
+                                            job_applications)
+    print_search_results(found_applications)
+  elif choice == '7':
     print("Exiting...")
     break
   else:
